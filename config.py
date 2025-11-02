0af3b936e08687fb8a1a1cbdca0d9608b698956d
Артем <mail@zveronline.ru>
2025-11-02 19:24:55 +0000

    build

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "0123456789abcdef0123456789abcdef",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "ya.ru"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
