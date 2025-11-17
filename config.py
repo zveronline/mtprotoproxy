a1916c7461f1660f3e862ed9edd5e680d9b84ac8
Артем <mail@zveronline.ru>
2025-11-17 13:20:44 +0000

    tls

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-TLS_DOMAIN = "ok.ru"
+TLS_DOMAIN = "google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
